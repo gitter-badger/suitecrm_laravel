f9f26121e9a22f679618ed98d29b1b6447e910d4
Glenn Hermans <glhermans@gmail.com>
2015-09-22 04:43:59 +0200

    Added portal language file

---
+++ resources/lang/en/advancedopenportal.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines for the portal
+	|
+	*/
+
+	"portal_name" => "Advanced Open Portal",
+	"login_required" => "You must be logged in to use the portal",
+	"no_portal_account" => "This account does not have portal access",
+	"portal_user_blocked" => "This portal account has been disabled",	
+
+	"create_case" => "Create a new case",
+	"list_cases" => "List cases",	
+	"case_status" => "Status",
+	"case_status_all" => "All",	
+	"case_status_open" => "Open",
+	"case_status_closed" => "Closed",
+	"case_state" => "State",
+	"case_number" => "Number",
+	"case_subject" => "Subject",
+	"case_created" => "Created",		
+	"case_last_update" => "Last Update",
+	"case_type" => "Type",	
+	"case_priority" => "Priority",	
+	"case_description" => "Description",
+	"case_add_file" => "Add file",	
+	"case_add_another_file" => "Add another file",	
+	"case_remove_file" => "X",	
+	"case_files" => "Files",
+	"case_update_on" => "On",
+	"case_update_said" => "Said",	
+	"case_update_placeholder" => "Reply here",	
+	"case_update_send_reply" => "Send reply",
+   "case_update_sending" => "Sending",	
+   "save" => "Save",
+   "close" => "Close", 
+   "reopen" => "Reopen", 
+   "case_created_by" => "Raised by",                   
+   "case_raised" => "Raised by",			
+   "case_raised_on" => "Raised on",
+   "own_cases" => "My cases",
+   "search" => "Search",      
+   
+						
+];
