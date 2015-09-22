50a253dbea06c737b7c621530fd35d7a52c238bf
Glenn Hermans <glhermans@gmail.com>
2015-09-22 03:55:10 +0200

    Created config file to store the general crm credentials #2

---
+++ config/suitecrm.php
@@ -0,0 +1,38 @@
+<?php
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Connection type 
+	|--------------------------------------------------------------------------
+	|
+	| Choose if we make the connection to suitecrm with nuSOAP or curl
+	|
+	*/
+
+	'url_type' => "",
+
+	/*
+	|--------------------------------------------------------------------------
+	| SuiteCRM Application URL
+	|--------------------------------------------------------------------------
+	|
+	| This URL is used to make a connection to the crm
+	|
+	*/
+
+	'url' => 'https://suitecrm.url',
+	
+	/*
+	|--------------------------------------------------------------------------
+	| SuiteCRM user credentials
+	|--------------------------------------------------------------------------
+	|
+	| This URL is used to make a connection to the crm
+	|
+	*/
+
+	'portal_username' => 'admin',
+	'portal_password' => 'admin123',	
+		
+];
