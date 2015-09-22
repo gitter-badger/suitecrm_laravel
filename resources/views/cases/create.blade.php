ecee262ede9b271bf29effc155e800d08e6a9cb7
Glenn Hermans <glhermans@gmail.com>
2015-09-22 06:33:16 +0200

    Changed language vars

--- resources/views/cases/create.blade.php
+++ resources/views/cases/create.blade.php
@@ -3,30 +3,29 @@
 <form class="">
 
  <div class="form-group">
-  <label class="form-label">Type</label>
-  <select name="case_type" class="form-control">
+  <label for="case_type" class="form-label"><?php echo Lang::get('aop.case_type');?></label>
+  <select name="case_type" id="case_type" class="form-control">
    <option>Select type</option>  
   </select>
  </div>
  
   <div class="form-group">
-  <label class="form-label">Subject</label>
-  <input name="case_subject" class="form-control">
+  <label for="case_subject" class="form-label"><?php echo Lang::get('aop.case_subject');?></label>
+  <input name="case_subject" id="case_subject" class="form-control">
  </div>
 
  <div class="form-group">
-  <label class="form-label">Type</label>
-  <textarea name="case_description" class="form-control"> </textarea>
+  <label for="case_message" class="form-label"><?php echo Lang::get('aop.case_description');?></label>
+  <textarea name="case_message" id="case_message" class="form-control" rows="5"></textarea>
  </div>
  
    <div class="form-group">
-    <label for="exampleInputFile">File input</label>
-    <input type="file" id="exampleInputFile" class="form-control">
-    <p class="help-block">Example block-level help text here.</p>
+    <label for="case_file"><?php echo Lang::get('aop.case_add_file');?></label>
+    <input type="file" id="case_file" class="form-control">
   </div>
  
   <div class="form-group">
-   <button type="submit" name="case_submit" class="btn btn-custom">Create ticket</button>
+   <button type="submit" name="case_submit" class="btn btn-custom"><?php echo Lang::get('aop.create_case');?></button>
  </div>
 </form>
 
